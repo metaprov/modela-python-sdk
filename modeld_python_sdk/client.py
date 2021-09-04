73ebbb28d9995b449794bb913360ca1a0405573f
tmeshorer <tmeshorer@hotmail.com>
2021-09-04 09:17:16 -0700

    fixed the client. upgrade version

--- modeld_python_sdk/client.py
+++ modeld_python_sdk/client.py
@@ -1,139 +1,275 @@
-import grpc
+import grpc # type: ignore
 
-from proto import prediction_server_pb2, prediction_server_pb2_grpc
+from grpc import (  # type: ignore
+    UnaryUnaryClientInterceptor,
+    UnaryStreamClientInterceptor,
+    StreamUnaryClientInterceptor,
+    StreamStreamClientInterceptor
+)
+
+from github.com.metaprov.modeldapi.services.grpcinferenceservice.v1 import grpcinferenceservice_pb2
+from github.com.metaprov.modeldapi.services.grpcinferenceservice.v1 import grpcinferenceservice_pb2_grpc
+
+from typing import Dict, Optional, Union, Sequence, List
 
 SERVER_ADDRESS = '0.0.0.0'
 PORT = 8080
 
 
-class PredictionServerClient(object):
-    def __init__(self,host,port=3000):
+from dataclasses import dataclass
+
+########################################################
+# Server data
+########################################################
+
+@dataclass
+class ServerInfo:
+    name: str
+
+@dataclass
+class Metric:
+    name: str
+    value: float
+
+
+@dataclass
+class ModelInfo:
+    name: str
+    version: str
+    product: str
+    trainingMetrics: List[Metric]
+    trainingScore: float
+    status: str
+    canary: bool
+    shadow: bool
+    traffic: float
+    filter:str
+    rank:int
+    logPath: str
+    p95: float
+    p99: float
+    lastPrediction: int
+    dailyPredictionAvg: float
+
+@dataclass
+class ColumnInfo:
+    name: str
+    type: str
+    datasetMin: float
+    datasetMean: float
+    datasetStdDev: float
+    datasetMax: float
+
+
+@dataclass
+class SchemaInfo:
+    columns: List[ColumnInfo]
+
+@dataclass
+class PredictorInfo:
+    name: str
+    task: str
+    models: List[ModelInfo]
+    schema: SchemaInfo
+
+@dataclass
+class OnePrediction:
+    payload:str
+
+@dataclass
+class Predictions:
+    payload:str # result as json
+    items: List[OnePrediction]
+
+
+#######################################################
+# Model data
+#######################################################
+
+
+class ModeldGrpcClient(object):
+    def __init__(self,
+                 host,
+                 port=3000,
+                 interceptors: Optional[List[Union[
+                     UnaryUnaryClientInterceptor,
+                     UnaryStreamClientInterceptor,
+                     StreamUnaryClientInterceptor,
+                     StreamStreamClientInterceptor]]] = None
+                 ):
         """Initializer.
            Creates a gRPC channel for connecting to the server.
            Adds the channel to the generated client stub.
+
         Arguments:
             None.
 
         Returns:
             None.
         """
-        self.channel = grpc.insecure_channel(f'{host}:{port}')
-        self.stub = prediction_server_pb2_grpc.PredictionServerStub(self.channel)
+        self._channel = grpc.insecure_channel(f'{host}:{port}')
 
+        if interceptors:
+            self._channel = grpc.intercept_channel(  # type: ignore
+                self._channel, *interceptors)
 
-    def predict(self,name:str,columns:str,features:str,full:bool) -> str:
-        request = prediction_server_pb2.PredictionRequest(
-            name=name,columns=columns,features=features,full=full
-        )
+        self._stub = grpcinferenceservice_pb2_grpc.GRPCInferenceServiceStub(self._channel)
 
+    def close(self):
+        """Closes Dapr runtime gRPC channel."""
+        if self._channel:
+            self._channel.close()
+
+    def __del__(self):
+        self.close()
+
+    def __enter__(self) -> 'ModeldGrpcClient':
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        self.close()
+
+    def is_alive(self) -> bool:
+        """Check if the service is alive
+
+        Arguments:
+           None
+
+        Returns:
+           bool; True if the predictor is alive
+        """
+        request = grpcinferenceservice_pb2.ServiceLiveRequest()
         try:
-            response = self.stub.Predict(request)
-            return response
+            response = self._stub.ServiceLive(request)
+            return response.live
+        except grpc.RpcError as err:
+            print(err.details())  # pylint: disable=no-member
+            print('{}, {}'.format(err.code().name, err.code().value))  # pylint: disable=no-member
+            return False
+
+    def is_ready(self) -> bool:
+        """Check if the service is ready
+
+        Arguments:
+             None
+
+        Returns:
+            bool; True if the predictor is alive
+        """
+        request = grpcinferenceservice_pb2.ServiceReadyRequest()
+        try:
+            response = self._stub.ServerReady(request)
+            return response.ready
+        except grpc.RpcError as err:
+            print(err.details())  # pylint: disable=no-member
+            print('{}, {}'.format(err.code().name, err.code().value))  # pylint: disable=no-member
+            return False
+
+    def model_ready(self,name:str,version:str) -> bool:
+        """Check if a specific model is ready
+
+        Arguments:
+             None
+
+        Returns:
+            bool; True if the predictor is alive
+        """
+
+        request = grpcinferenceservice_pb2.ModelReadyRequest()
+        request.name    = name
+        request.version = version
+        try:
+            response = self._stub.ModelReady(request)
+            return response.ready
+        except grpc.RpcError as err:
+            print(err.details())  # pylint: disable=no-member
+            print('{}, {}'.format(err.code().name, err.code().value))  # pylint: disable=no-member
+            return False
+
+    def server_metadata(self) -> ServerInfo:
+        """Answer the server metadata info
+
+        Arguments:
+             None
+
+        Returns:
+            bool; True if the predictor is alive
+        """
+
+        request = grpcinferenceservice_pb2.ServerMetadataRequest()
+        try:
+            response = self._stub.ServerMetadata(request)
+            return response.ready
         except grpc.RpcError as err:
             print(err.details())  # pylint: disable=no-member
             print('{}, {}'.format(err.code().name, err.code().value))  # pylint: disable=no-member
 
 
-    def batch_predict(self,url:str) -> str:
-        """Make a batch prediction.
-                Arguments:
-                    url: The resource name of a user.
-
-                Returns:
-                    a url for the result.
-                """
-        request = prediction_server_pb2.BatchPredictRequest(
-            url=url
-        )
-
+    def model_metadata(self,name:str,version:str) -> ModelInfo:
+        request = grpcinferenceservice_pb2.ModelMetadataRequest()
+        request.name = name
+        request.version = version
         try:
-            response = self.stub.BatchPredict(request)
-            return response
+            response = self._stub.ModelMetadata(request)
+            return response.ready
         except grpc.RpcError as err:
             print(err.details())  # pylint: disable=no-member
             print('{}, {}'.format(err.code().name, err.code().value))  # pylint: disable=no-member
+            return False
 
 
-
-    def get_product_metadata(self) -> str:
-        """Gets the product metadata.
-                Arguments:
-                    name: The resource name of a user.
-
-                Returns:
-                    a json string describing the product
-                """
-
-        request = prediction_server_pb2.GetProductRequest()
+    def get_predictor(self) -> PredictorInfo:
+        request = grpcinferenceservice_pb2.GetPredictorRequest()
         try:
-            response = self.stub.GetProduct(request)
-            return response.content
+            response = self._stub.GetPredictor(request)
+            return response.item
         except grpc.RpcError as err:
             print(err.details())  # pylint: disable=no-member
             print('{}, {}'.format(err.code().name, err.code().value))  # pylint: disable=no-member
+            raise err
 
-    def get_schema_metadata(self) -> str:
-        """Gets the schema metadata
-                Arguments:
-                    None
+    def get_model(self,predictor,name) -> ModelInfo:
+        """Answer the model information
 
-                Returns:
-                    a json string describing the schema for this predictor
-           """
-        request = prediction_server_pb2.GetSchemaRequest()
+        Arguments:
+             name - the name of the model
+
+        Returns:
+            bool; True if the predictor is alive
+        """
+
+        request = grpcinferenceservice_pb2.GetModelRequest()
+        request.predictorName = predictor
+        request.name          = name
         try:
-            response = self.stub.GetSchema(request)
-            return response.content
+            response = self._stub.GetModel(request)
+            return response.item
         except grpc.RpcError as err:
             print(err.details())  # pylint: disable=no-member
             print('{}, {}'.format(err.code().name, err.code().value))  # pylint: disable=no-member
+            raise err
 
-    def get_dataset_metadata(self) -> str:
-        """Gets the dataset metadata.
-                Arguments:
-                    None
 
-                Returns:
-                    a json string describing the dataset for this predictor
-                """
-        request = prediction_server_pb2.GetSchemaRequest()
+    def predict(self,
+                payload:str,
+                name="",
+                validate=False,
+                explain=False,
+                format="json",
+                labeled=False,
+                metrics=None) -> Predictions:
+        request = grpcinferenceservice_pb2.PredictRequest()
+        request.name      = name
+        request.validate  = validate
+        request.explain   = explain
+        request.format    = format
+        request.payload   = payload
+        request.labeled   = labeled
+        request.metrics.extend(metrics)
         try:
-            response = self.stub.GetSchema(request)
-            return response.content
+            response = self._stub.Predict(request)
+            return response.items
         except grpc.RpcError as err:
             print(err.details())  # pylint: disable=no-member
             print('{}, {}'.format(err.code().name, err.code().value))  # pylint: disable=no-member
-
-    def get_model(self) -> str:
-        """Gets the model metadata
-                Arguments:
-                    None
-
-                Returns:
-                    a json string describing the model for this predictor
-                """
-        request = prediction_server_pb2.GetSchemaRequest()
-        try:
-            response = self.stub.GetSchema(request)
-            return response.content
-        except grpc.RpcError as err:
-            print(err.details())  # pylint: disable=no-member
-            print('{}, {}'.format(err.code().name, err.code().value))  # pylint: disable=no-member
-
-
-    def get_stat(self) -> str:
-        """Gets a user.
-                Arguments:
-                    None
-                Returns:
-                    a json statistics string
-                """
-        request = prediction_server_pb2.GetStatRequest()
-        try:
-            response = self.stub.GetStat(request)
-            return response.content
-        except grpc.RpcError as err:
-            print(err.details())  # pylint: disable=no-member
-            print('{}, {}'.format(err.code().name, err.code().value))  # pylint: disable=no-member
-
-
+            raise err
--- proto/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for modeld-python-client."""
-
-__author__ = """modeld"""
-__email__ = 'info@modeld.io'
-__version__ = '0.1.0'
